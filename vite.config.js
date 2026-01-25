3c30f2b19bc4e2d3033d133909964e0b9f918ea4
Nathan Warner <nathan@frcv.net>
2026-01-25 10:24:17 -0700

    update Vite config base path and add CNAME for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cached-cards/',
+  base: '/',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
